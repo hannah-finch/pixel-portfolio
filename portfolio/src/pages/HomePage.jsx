36114e7798e33b2818ff1be46c5ad896a74a7e61
hannahschwen <hannahschwen@gmail.com>
2026-04-03 10:35:05 -0400

    further game engine optimizations, background tiling

--- portfolio/src/pages/HomePage.jsx
+++ portfolio/src/pages/HomePage.jsx
@@ -5,7 +5,6 @@ function HomePage() {
   // Assets
   const meIdle = "./src/assets/animations/me_idle_front.gif";
   const meItem = "./src/assets/animations/me_item_front.gif";
-  const meWalkFront = "./src/assets/animations/me_walk_front.gif";
   const meWalkRight = "./src/assets/animations/me_walk_right.gif";
   const meWalkLeft = "./src/assets/animations/me_walk_left.gif";
 
@@ -25,7 +24,75 @@ function HomePage() {
   const fgRef = useRef(null);
   const ffgRef = useRef(null);
 
-  const speed = 300; // pixels per second
+  const speed = 400; // pixels per second
+  const IMAGE_WIDTH = 4864; // width in pixels of the actual background images, super important for tiling
+
+  // Background image assets
+  const bgImg = "./src/assets/animations/background_back.png";
+  const midImg = "./src/assets/animations/background.png";
+  const fgImg = "./src/assets/animations/background_front.gif";
+  const ffgImg = "./src/assets/animations/background_frontfront.gif";
+
+  const createLayer = (ref, img, zIndex) => (
+    <div
+      ref={ref}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: `${IMAGE_WIDTH}px`,
+        height: "100%",
+        willChange: "transform",
+        zIndex: zIndex,
+        opacity: .94,
+      }}
+    >
+      {/* LEFT tile */}
+      <div
+        style={{
+          position: "absolute",
+          width: `${IMAGE_WIDTH}px`,
+          height: "100%",
+          left: `-${IMAGE_WIDTH}px`,
+          top: 0,
+          backgroundImage: `url(${img})`,
+          backgroundRepeat: "repeat-x",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+
+      {/* CENTER tile */}
+      <div
+        style={{
+          position: "absolute",
+          width: `${IMAGE_WIDTH}px`,
+          height: "100%",
+          left: 0,
+          top: 0,
+          backgroundImage: `url(${img})`,
+          backgroundRepeat: "repeat-x",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+
+      {/* RIGHT tile */}
+      <div
+        style={{
+          position: "absolute",
+          width: `${IMAGE_WIDTH}px`,
+          height: "100%",
+          left: `${IMAGE_WIDTH}px`,
+          top: 0,
+          backgroundImage: `url(${img})`,
+          backgroundRepeat: "repeat-x",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+    </div>
+  );
 
   const animate = (time) => {
     if (!isMoving.current) return;
@@ -33,27 +100,26 @@ function HomePage() {
     let delta = time - lastTimeRef.current;
     lastTimeRef.current = time;
 
-    // Clamp big jumps
-    if (delta > 50) delta = 50;
+    if (delta > 50) delta = 50; // avoids big jumps
 
     const movement = speed * direction.current * (delta / 1000);
     positionRef.current += movement;
 
-    const pos = positionRef.current;
+    const apply = (ref, multiplier) => {
+      if (!ref.current) return;
 
-    // Move layers (parallax)
-    if (bgRef.current) {
-      bgRef.current.style.transform = `translateX(${pos * 0.4}px)`;
-    }
-    if (midRef.current) {
-      midRef.current.style.transform = `translateX(${pos * 0.6}px)`;
-    }
-    if (fgRef.current) {
-      fgRef.current.style.transform = `translateX(${pos * 0.8}px)`;
-    }
-    if (ffgRef.current) {
-      ffgRef.current.style.transform = `translateX(${pos}px)`;
-    }
+      const layerWidth = IMAGE_WIDTH;
+
+      const raw = positionRef.current * multiplier;
+      const wrapped = raw % layerWidth;
+
+      ref.current.style.transform = `translateX(${wrapped}px)`;
+    };
+
+    apply(bgRef, 0.1);
+    apply(midRef, 0.6);
+    apply(fgRef, 0.8);
+    apply(ffgRef, 1);
 
     animationRef.current = requestAnimationFrame(animate);
   };
@@ -84,189 +150,129 @@ function HomePage() {
   }, []);
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      {/* Whole world */}
-      <div
-        ref={worldRef}
-        style={{
-          position: "absolute",
-          width: "200%",
-          height: "100%",
-          top: 0,
-          left: 0,
-        }}
-      >
-        {/* Background (the far hills) */}
-        <div
-          ref={bgRef}
-          style={{
-            position: "absolute",
-            width: "200%",
-            height: "100%",
-            backgroundImage:
-              "url('./src/assets/animations/background_back.png')",
-            backgroundRepeat: "repeat-x",
-            backgroundPosition: "center",
-            willChange: "transform",
-          }}
-        />
-
-        {/* Midground */}
-        <div
-          ref={midRef}
-          style={{
-            position: "absolute",
-            width: "200%",
-            height: "100%",
-            backgroundImage: "url('./src/assets/animations/background.png')",
-            backgroundRepeat: "repeat-x",
-            backgroundPosition: "center",
-            opacity: 0.9,
-            willChange: "transform",
-          }}
-        />
-
-        {/* Foreground */}
-        <div
-          ref={fgRef}
-          style={{
-            position: "absolute",
-            width: "200%",
-            height: "100%",
-            pointerEvents: "none",
-            backgroundImage:
-              "url('./src/assets/animations/background_front.gif')",
-            backgroundRepeat: "repeat-x",
-            backgroundPosition: "center",
-            opacity: 0.9,
-            willChange: "transform",
-            zIndex: "1",
-          }}
-        />
-
-        {/* Very front foreground */}
-        <div
-          ref={ffgRef}
-          style={{
-            position: "absolute",
-            width: "200%",
-            height: "100%",
-            pointerEvents: "none",
-            backgroundImage:
-              "url('./src/assets/animations/background_frontfront.gif')",
-            backgroundRepeat: "repeat-x",
-            backgroundPosition: "center",
-            opacity: 0.9,
-            willChange: "transform",
-            zIndex: "1",
-          }}
-        />
-      </div>
-
-      {/* PLAYER (look, it's a mini-me!) */}
+    <>
       <div
         style={{
-          position: "absolute",
-          top: "47%",
-        }}
-      >
-        <img
-          src={meGifSrc}
-          onMouseEnter={() => setMeGifSrc(meWalkFront)}
-          onMouseLeave={() => setMeGifSrc(meIdle)}
-        />
-      </div>
-
-      <h1
-        style={{
-          fontSize: "8vw",
-          position: "absolute",
-          top: "20px",
-          width: "100%",
-          textAlign: "center",
-          color: "var(--light-gray)",
-          zIndex: -1,
-        }}
-      >
-        Hannah B Finch
-      </h1>
-
-      <h1
-        style={{
-          fontSize: "4vw",
-          position: "absolute",
-          bottom: "20px",
-          color: "var(--light-gray)",
-          backgroundColor: "#151515df",
-          padding: "0 20px 10px",
-          zIndex: 1,
-        }}
-      >
-        Designer / Developer
-      </h1>
-      <div
-        style={{
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
           position: "relative",
-          zIndex: "2",
-          width: "300px",
-          top: "50%",
-          transform: "translateY(80px)",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          gap: "64px",
         }}
       >
+        {/* World */}
+        <div
+          ref={worldRef}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: 0,
+            transform: "translateY(-50%)",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {createLayer(bgRef, bgImg, 0)}
+          {createLayer(midRef, midImg, 0)}
+          {createLayer(fgRef, fgImg, 1)}
+          {createLayer(ffgRef, ffgImg, 1)}
+
+          {/* PLAYER (look, it's a mini-me!) */}
+          <img
+            src={meGifSrc}
+            style={{
+              position: "relative",
+              top: "50px",
+            }}
+          />
+        </div>
+
+        <h1
+          style={{
+            fontSize: "8vw",
+            position: "absolute",
+            top: "20px",
+            width: "100%",
+            textAlign: "center",
+            color: "var(--light-gray)",
+            zIndex: -1,
+          }}
+        >
+          Hannah B Finch
+        </h1>
+
+        <h1
+          style={{
+            fontSize: "4vw",
+            position: "absolute",
+            bottom: "20px",
+            color: "var(--light-gray)",
+            backgroundColor: "#151515df",
+            padding: "0 20px 10px",
+            zIndex: 1,
+          }}
+        >
+          Designer / Developer
+        </h1>
         <div
           style={{
+            position: "relative",
+            zIndex: "2",
+            width: "300px",
+            top: "50%",
+            transform: "translateY(80px)",
             display: "flex",
-            justifyContent: "space-between",
-            width: "100%",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "64px",
           }}
         >
           <div
-            className="icon-container"
-            onMouseDown={() => startMoving(1, meWalkLeft)}
-            onMouseUp={stopMoving}
-            onMouseLeave={stopMoving}
-            onTouchStart={() => startMoving(1, meWalkLeft)}
-            onTouchEnd={stopMoving}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
           >
-            &lt;
-          </div>
+            <div
+              className="icon-container"
+              onMouseDown={() => startMoving(1, meWalkLeft)}
+              onMouseUp={stopMoving}
+              onMouseLeave={stopMoving}
+              onTouchStart={() => startMoving(1, meWalkLeft)}
+              onTouchEnd={stopMoving}
+            >
+              &lt;
+            </div>
 
-          <div
-            className="icon-container"
-            onMouseDown={() => startMoving(-1, meWalkRight)}
-            onMouseUp={stopMoving}
-            onMouseLeave={stopMoving}
-            onTouchStart={() => startMoving(-1, meWalkRight)}
-            onTouchEnd={stopMoving}
-          >
-            &gt;
+            <div
+              className="icon-container"
+              onMouseDown={() => startMoving(-1, meWalkRight)}
+              onMouseUp={stopMoving}
+              onMouseLeave={stopMoving}
+              onTouchStart={() => startMoving(-1, meWalkRight)}
+              onTouchEnd={stopMoving}
+            >
+              &gt;
+            </div>
           </div>
+          <Link to="/work">
+            <div
+              className="button"
+              onMouseEnter={() => setMeGifSrc(meItem)}
+              onMouseLeave={() => setMeGifSrc(meIdle)}
+            >
+              Look what I made!
+            </div>
+          </Link>
         </div>
-        <Link to="/work">
-          <div
-            className="button"
-            onMouseEnter={() => setMeGifSrc(meItem)}
-            onMouseLeave={() => setMeGifSrc(meIdle)}
-          >
-            Look what I made!
-          </div>
-        </Link>
       </div>
-    </div>
+    </>
   );
 }
 
